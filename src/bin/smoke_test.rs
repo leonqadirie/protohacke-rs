6b5b88776fd1004b14382e3feae6c9d95eb35218
Leon Qadirie <leon.qadirie@nulltree.xyz>
2024-01-10 23:28:43 +0100

    Add solution for echo server

---
+++ src/bin/smoke_test.rs
@@ -0,0 +1,47 @@
+use anyhow::{Context, Result};
+use std::net::SocketAddr;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+async fn handle_client(mut socket: TcpStream, addr: SocketAddr) -> Result<()> {
+    let mut buf = [0; 1024];
+
+    loop {
+        let n = socket
+            .read(&mut buf)
+            .await
+            .context(format!("Failed to read data from socket {:?}", addr))?;
+
+        if n == 0 {
+            return Ok(());
+        }
+
+        socket
+            .write_all(&buf[..n])
+            .await
+            .context(format!("Failed to write data to socket {:?}", addr))?;
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4242")
+        .await
+        .context("couldn't create TcpListener")?;
+
+    loop {
+        let (socket, addr) = match listener.accept().await {
+            Ok((socket, addr)) => (socket, addr),
+            Err(e) => {
+                println!("Accept error = {:?}", e);
+                continue;
+            }
+        };
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(socket, addr).await {
+                println!("Error handling client: {:?} {:?}", addr, e);
+            };
+        });
+    }
+}
